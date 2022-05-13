34d274a9edd4c55a9c52948122a74860c7010bbf
PandeyUjjwal <47017598+PandeyUjjwal@users.noreply.github.com>
2022-05-13 17:50:56 +0530

    Add files via upload

---
+++ precache-manifest.00372d345f4d50707076cd4547a519d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c3b4df83ceb5c32d365ffe076e301d",
+    "url": "/Ujjwal_Firegram/index.html"
+  },
+  {
+    "revision": "69264abe638cae0355f6",
+    "url": "/Ujjwal_Firegram/static/css/main.e3bd3a6e.chunk.css"
+  },
+  {
+    "revision": "fe426ce931bbe3c75ec2",
+    "url": "/Ujjwal_Firegram/static/js/2.e74f08f7.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/Ujjwal_Firegram/static/js/2.e74f08f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69264abe638cae0355f6",
+    "url": "/Ujjwal_Firegram/static/js/main.7ab4aeda.chunk.js"
+  },
+  {
+    "revision": "103af05cfeefe9bf5446",
+    "url": "/Ujjwal_Firegram/static/js/runtime-main.2d270a10.js"
+  }
+]);
